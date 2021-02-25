967f55651cd0b14d28367b4cebcf73c443b2d03e
Mystic Kitty <djabranton@gmail.com>
2021-02-25 15:46:47 -0800

    Hooked up server and created initial schema files!

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+
+// const routes = require('./routes'); //old api
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +28,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes); //old api
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
