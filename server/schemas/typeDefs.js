a93e0dae877c449222a4f67d5b4868c2de109455
Mystic Kitty <djabranton@gmail.com>
2021-02-25 17:24:25 -0800

    Added typeDefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,43 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+type mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook: (bookId: Int!): User
+}  
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
 // export the typeDefs
 module.exports = typeDefs;
+
+// saveBook([author]: String, description: String, title: String, bookId: Int, image: string, link: String): User
