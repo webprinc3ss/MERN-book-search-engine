b0575f0eaa854dcc755c27bcfcff3dc21c9205e9
Mystic Kitty <djabranton@gmail.com>
2021-02-27 11:03:13 -0800

    Commit

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -4,7 +4,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
