c693f5fb46ac0483ef28bfa7053abfb60d9ed332
Mystic Kitty <djabranton@gmail.com>
2021-02-26 21:22:49 -0800

    Working on SavedBooks.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,6 +9,7 @@ const resolvers = {
             if (context.user) {
                 const userData = await User.findOne({ _id: context.user._id })
                     .select('-__v -password')
+                    .populate('books')
                 return userData
             }
 
